b52b6ba4b69f8ed0756afde5be6f065ed7737a50
Michael <mwrowan89@gmail.com>
2024-04-01 14:52:11 -0600

    Fixed calculator component

--- src/components/CostCalculator.vue
+++ src/components/CostCalculator.vue
@@ -1,56 +1,39 @@
 <template>
-    <h1>Cost Calculator</h1>
-</template>
-
-<script>
-let expenses = {}; 
-let people = {};
-
-// Function to add an expense
-function addExpense(expenseName, amount, participants) {
-    expenses[expenseName] = { amount, participants };
-    updateAmounts();
-}
-
-// Function to add a person
-function addPerson(personName) {
-    people[personName] = {};
-    updateAmounts();
-}
-
-// Function to update amounts owed
-function updateAmounts() {
-    for (let person in people) {
-        people[person].totalOwed = 0;
-        for (let expense in expenses) {
-            let { amount, participants } = expenses[expense];
-            if (participants.includes(person)) {
-                people[person].totalOwed += amount / participants.length;
-            }
-        }
+    <div>
+      <person-input @add-person="addPerson"></person-input>
+      <expense-input :people="people" @add-expense="addExpense"></expense-input>
+      <person-list :people="people"></person-list>
+      <expense-list :expenses="expenses"></expense-list>
+    </div>
+  </template>
+  
+  <script>
+  import ExpenseInput from './ExpenseInput.vue';
+  import PersonInput from './PersonInput.vue';
+  import ExpenseList from './ExpenseList.vue';
+  import PersonList from './PersonList.vue';
+  
+  export default {
+    components: {
+      ExpenseInput,
+      PersonInput,
+      ExpenseList,
+      PersonList
+    },
+    data() {
+      return {
+        people: [],
+        expenses: []
+      }
+    },
+    methods: {
+      addPerson(personName) {
+        this.people.push(personName);
+      },
+      addExpense(expenseName, amount, participants) {
+        this.expenses.push({ expenseName, amount, participants });
+      }
     }
-}
-
-// Function to exclude a person from an expense
-function excludePersonFromExpense(expenseName, personName) {
-    if (expenses[expenseName]) {
-        expenses[expenseName].participants = expenses[expenseName].participants.filter(participant => participant !== personName);
-        updateAmounts();
-    }
-}
-
-// Example usage
-addPerson("Alice");
-addPerson("Bob");
-addPerson("Charlie");
-
-addExpense("AirBnb", 300, ["Alice", "Bob", "Charlie"]);
-addExpense("CarRental", 150, ["Alice", "Bob"]);
-addExpense("Skiing", 200, ["Alice"]);
-
-excludePersonFromExpense("Skiing", "Alice");
-
-console.log(people);
-
-
-</script>
+  }
+  </script>
+  
