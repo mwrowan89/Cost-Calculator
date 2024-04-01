de7b236b1aaa3b67981b6f7d07d8598e0de8b2c6
Michael <mwrowan89@gmail.com>
2024-04-01 14:51:32 -0600

    Re-structured components

---
+++ src/components/PersonInput.vue
@@ -0,0 +1,25 @@
+// PersonInput.vue
+<template>
+  <div>
+    <input v-model="personName" type="text" placeholder="Person's Name">
+    <button @click="addPerson">Add Person</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      personName: ''
+    }
+  },
+  methods: {
+    addPerson() {
+      if (this.personName.trim() !== '') {
+        this.$emit('add-person', this.personName);
+        this.personName = '';
+      }
+    }
+  }
+}
+</script>
