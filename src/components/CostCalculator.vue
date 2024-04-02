a63bcaf9273f1875edfc5eadb66737ffe90d8679
Michael <mwrowan89@gmail.com>
2024-04-02 14:28:06 -0600

    Added styling to page

--- src/components/CostCalculator.vue
+++ src/components/CostCalculator.vue
@@ -1,39 +1,39 @@
 <template>
-    <div>
-      <person-input @add-person="addPerson"></person-input>
-      <expense-input :people="people" @add-expense="addExpense"></expense-input>
-      <person-list :people="people"></person-list>
-      <expense-list :expenses="expenses"></expense-list>
-    </div>
-  </template>
+  <div>
+    <person-input @add-person="addPerson"></person-input>
+    <expense-input :people="people" @add-expense="addExpense"></expense-input>
+    <person-list :people="people"></person-list>
+    <expense-list :expenses="expenses"></expense-list>
+  </div>
+</template>
   
-  <script>
-  import ExpenseInput from './ExpenseInput.vue';
-  import PersonInput from './PersonInput.vue';
-  import ExpenseList from './ExpenseList.vue';
-  import PersonList from './PersonList.vue';
-  
-  export default {
-    components: {
-      ExpenseInput,
-      PersonInput,
-      ExpenseList,
-      PersonList
+<script>
+import ExpenseInput from './ExpenseInput.vue';
+import PersonInput from './PersonInput.vue';
+import ExpenseList from './ExpenseList.vue';
+import PersonList from './PersonList.vue';
+
+export default {
+  components: {
+    ExpenseInput,
+    PersonInput,
+    ExpenseList,
+    PersonList
+  },
+  data() {
+    return {
+      people: [],
+      expenses: []
+    }
+  },
+  methods: {
+    addPerson(personName) {
+      this.people.push(personName);
     },
-    data() {
-      return {
-        people: [],
-        expenses: []
-      }
-    },
-    methods: {
-      addPerson(personName) {
-        this.people.push(personName);
-      },
-      addExpense(expenseName, amount, participants) {
-        this.expenses.push({ expenseName, amount, participants });
-      }
+    addExpense(expenseName, amount, participants) {
+      this.expenses.push({ expenseName, amount, participants });
     }
   }
-  </script>
+}
+</script>
   
--- src/components/ExpenseList.vue
+++ src/components/ExpenseList.vue
@@ -1,25 +1,27 @@
 <template>
-    <div>
-      <h2>Expenses</h2>
-      <ul>
-        <li v-for="(expense, index) in expenses" :key="index">
-          <strong>{{ expense.expenseName }}</strong>: ${{ expense.amount }}
-          <span v-if="expense.participants.length > 0">
-            (Participants: {{ expense.participants.join(', ') }})
-          </span>
-          <button @click="removeExpense(index)">Remove</button>
-        </li>
-      </ul>
-    </div>
-  </template>
+  <div class="expense-input">
+    <h2>Expenses</h2>
+    <ul>
+      <li v-for="(expense, index) in expenses" :key="index">
+        <strong>{{ expense.expenseName }}</strong>: ${{ expense.amount }}
+        <span v-if="expense.participants.length > 0">
+          (Participants: {{ expense.participants.join(', ') }})
+        </span>
+        <button @click="removeExpense(index)">Remove</button>
+      </li>
+    </ul>
+  </div>
+</template>
   
-  <script>
-  export default {
-    props: ['expenses'],
-    methods: {
-      removeExpense(index) {
-        this.$emit('remove-expense', index);
-      }
+<script>
+export default {
+  props: ['expenses'],
+  methods: {
+    removeExpense(index) {
+      this.$emit('remove-expense', index);
     }
   }
-  </script>
+}
+</script>
+
+<style scoped></style>
